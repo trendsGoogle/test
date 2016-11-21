9e2cdfed8b0d9fd061a1960d4e2f0d3e956394b5
Boris Snigirev <bsnigirev@google.com>
2016-11-21 16:25:58 +0200

    update code

--- test_node.js
+++ test_node.js
@@ -11,6 +11,8 @@ var server = http.createServer(function(req, res) {
         ' <script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/815_RC03/embed_loader.js"></script> <script type="text/javascript"> trends.embed.renderWidget("US_cu_5xacMlYBAAAtpM_en", "fe_int_over_time_6e567247-7423-4d56-9977-2f68e76cbbdd", {"guestPath":"https://trends.sandbox.google.com:443/trends/embed/"}); </script>'+
         '<p>Server: </p>' +
         ' <script type="text/javascript"> trends.embed.renderWidget("US_cu_5xacMlYBAAAtpM_en", "fe_geo_chart_3b9ce31b-cd31-4e2d-96b6-35cb21e01a06", {"guestPath":"https://trends.sandbox.google.com:443/trends/embed/"}); </script> '+
+        '<p>Server: </p>' +
+        '<script type="text/javascript"> trends.embed.renderWidget("election2016", "fe_line_chart_fcb71257-d6aa-4092-a269-77566ce4ac04", {"guestPath":"https://trends.sandbox.google.com:443/trends/embed/"}); </script> '+
         ' </body>'+
         '</html>');
 });
