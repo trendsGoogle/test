36e7e8f03605bdee71fd4029c54a202e2cccbf82
Boris Snigirev <bsnigirev@google.com>
2016-11-22 17:50:08 +0200

    update code

--- test_node.js
+++ test_node.js
@@ -34,7 +34,7 @@ var server = http.createServer(function(req, res) {
         '<p>Server: </p>',
         '<script type="text/javascript"> trends.embed.renderWidget("US_cu_MK0gIFcBAABHDM_en", "fe_list_82505dde-cd2d-4895-9393-561cbd4c1ce6", {"guestPath":"https://trends.sandbox.google.com:443/trends/embed/"}); </script>',
         '<p>Server Prod: </p>',
-        '<script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"/m/02y17j","geo":"","time":"all"},{"keyword":"/m/09gms","geo":"","time":"all"},{"keyword":"/m/012y1_","geo":"","time":"all"}],"category":0,"property":""}, {"exploreQuery":"q=%2Fm%2F02y17j,%2Fm%2F09gms,%2Fm%2F012y1_&date=all"}); </script> ',
+        '<script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"/m/02mjmr","geo":"US","time":"2015-01-01 2015-12-31"},{"keyword":"/m/0d06m5","geo":"US","time":"2015-01-01 2015-12-31"},{"keyword":"/m/0cqt90","geo":"US","time":"2015-01-01 2015-12-31"}],"category":0,"property":""}, {"exploreQuery":"q=%2Fm%2F02mjmr,%2Fm%2F0d06m5,%2Fm%2F0cqt90&date=2015-01-01%202015-12-31&geo=US"}); </script> ',
         '</body>',
         '</html>'
       ].join(''));
